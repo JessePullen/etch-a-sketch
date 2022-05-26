3644733f0d0f836b84584ec001b7eefa94af0a53
JessePullen <jessepullen@icloud.com>
2022-05-26 15:08:02 +1000

    Add createGrid function that dynamically creates a grid based on assigned row x col

---
+++ script.js
@@ -0,0 +1,24 @@
+const gridContainer = document.querySelector('.grid-container');
+
+let gridRows = '';
+let gridCount = 0;
+let gridWidth = 16;
+
+//Function that creates dynamic grid to specified row x col.
+//Creates unique element with class and adjusts 'grid-template-columns' CSS by width of grid.
+function createGrid() {
+    for (let i = 0; i < gridWidth; i++) {
+        gridRows += ' auto';
+        gridContainer.style.gridTemplateColumns = gridRows;
+
+        for (let j = 0; j < gridWidth; j++) {
+            gridCount++
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item-' + gridCount);
+            gridItem.textContent = gridCount;
+            gridContainer.appendChild(gridItem);
+        }    
+    }
+}
+
+createGrid();
