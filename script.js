d40a20c3abff918c8c42e6f9dc4f5937f1952a96
JessePullen <jessepullen@icloud.com>
2022-05-26 22:56:57 +1000

    Add random colour option for drawing. Add buttons to swtich drawing modes. Amend drawOnGrid function for new options

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const gridContainer = document.querySelector('.grid-container');
-const reset = document.querySelector('.reset')
-const changeSize = document.querySelector('.change-size')
+const reset = document.querySelector('.reset');
+const changeSize = document.querySelector('.change-size');
+const gradient = document.querySelector('.gradient');
+const random = document.querySelector('.random');
 
 let gridRows = '';
 let gridCount = 0;
 let gridWidth = 16;
+let drawType = 'gradient';
 
 //Function that creates dynamic grid to specified row x col.
 //Creates unique element with class and adjusts 'grid-template-columns' CSS by width of grid.
@@ -19,7 +22,7 @@ function createGrid() {
             gridItem.classList.add('grid-item-' + gridCount);
             gridItem.classList.add('grid-item');
             gridContainer.appendChild(gridItem);
-        }    
+        }
     }
 }
 
@@ -34,28 +37,42 @@ function removeGrid() {
     });
 }
 
-//Adds event listener to all grid items created. Uses class applied in createGrid.
+//Adds event listener to all grid items created. Changes grid item to a random colour or gradient.
 function drawOnGrid() {
     const gridItems = document.querySelectorAll('.grid-item');
-    
+
     gridItems.forEach((gridItem) => {
-        gridItem.addEventListener('mouseover', (e) => {
-            opacity = e.target.style.opacity;
-            
-            if (opacity == '') {
-                opacity = 0.1;
-            }
-            
-            e.target.style.opacity = parseFloat(opacity) + 0.175;
-            e.target.style.backgroundColor = 'black';
-        });
+        if (drawType == 'gradient') {
+            gridItem.addEventListener('mouseover', (e) => {
+                let opacity = e.target.style.opacity;
+
+                if (opacity == '') {
+                    opacity = 0.1;
+                }
+                e.target.style.opacity = parseFloat(opacity) + 0.175;
+                e.target.style.backgroundColor = 'black';
+            });       
+        } else {
+            gridItem.addEventListener('mouseover', (e) => {
+                e.target.style.opacity = 1;
+                e.target.style.backgroundColor = getRandomColour();
+            });
+        } 
     });
+}
+
+//
+function getRandomColour() {
+    let maxHex = 0xFFFFFF;
+    let randomNumber = Math.random() * maxHex;
+    randomNumber = Math.floor(randomNumber);
+    return randomColour = '#' + randomNumber.toString(16);
 }
 
 //Resets grid items to white
 function resetGrid() {
     const gridItems = document.querySelectorAll('.grid-item');
-    
+
     gridItems.forEach((gridItem) => {
         gridItem.style.backgroundColor = '';
         gridItem.style.opacity = '';
@@ -86,6 +103,16 @@ changeSize.addEventListener('click', () => {
     getWidth();
     removeGrid();
     runProgram();
+});
+
+gradient.addEventListener('click', () => {
+    drawType = 'gradient';
+    drawOnGrid();
+});
+
+random.addEventListener('click', () => {
+    drawType = 'random';
+    drawOnGrid();
 });
 
 runProgram();
